f58bd50485cd3283454370865d59abba188fc3a1
Mateus Magnus Pimentel Paape <mpaape.mp@gmail.com>
2026-08-09 00:21:54 -0300

    AUR-006: use writable Go cache in worker

--- tests/acceptance/AUR-006.sh
+++ tests/acceptance/AUR-006.sh
@@ -84,7 +84,9 @@ run_go_test() {
   set +e
   (
     cd -- "$run_dir" || exit 70
-    AURUMCODE_ROOT="$run_dir" GOTOOLCHAIN=local GOPROXY=off go test "$@"
+    mkdir -p -- "$run_dir/.go-cache" "$run_dir/.go-tmp" || exit 70
+    AURUMCODE_ROOT="$run_dir" GOTOOLCHAIN=local GOPROXY=off \
+      GOCACHE="$run_dir/.go-cache" GOTMPDIR="$run_dir/.go-tmp" go test "$@"
   ) >"$output" 2>&1
   local test_exit=$?
   set -e
